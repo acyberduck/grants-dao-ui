e9933971362139cd89b669095d1b43ddda345c80
max <39312833+fritzschoff@users.noreply.github.com>
2022-11-17 19:55:36 +0100

    fixing signing

--- queries/constants.ts
+++ queries/constants.ts
@@ -7,9 +7,9 @@ export const MEMBERS = [
     address: "0x4f370b4d03d2b46cce26f1aefe142708e03d7ffe",
   },
   {
-    displayName: "Mike#0714",
-    ens: "ensmaxis.eth",
-    address: "0xe1f02f7e90ea5f21d0ac6f12c659c3484c143b03",
+    displayName: "Maxxam#8383",
+    ens: "fritzschoff.eth",
+    address: "0x71226c538679eD4A72E803b3E2C93aD7403DA094",
   },
   {
     displayName: "cyberduck.eth",
@@ -17,31 +17,13 @@ export const MEMBERS = [
     address: "0xbf49b454818783d12bf4f3375ff17c59015e66cb s",
   },
   {
-    displayName: "joey#1760",
-    ens: "jocke.eth",
-    address: "0x86691c1dd5d567f8e73a46feb0ddd2b42e404b6b",
-  },
-  {
-    displayName: "beachmom__/#3781",
-    ens: undefined,
-    address: "0x5753f05A0C757F38f8365c22B8f9Bd6DA763D1F5",
-  },
-  {
     address: "0x1a207bEefC754735871CEEb4C506686F044B1c41",
     displayName: "ΔLΞXΔNDΞR",
   },
-  {
-    address: "0xF5a701D13Be654a534D14DE8Cd6a328293712405",
-    displayName: "DG",
-  },
-  {
-    address: "0x099DbFa498d0b7Fbdf8bC6eD9FaF868454022EC2",
-    displayName: "clΞm",
-    ens: "clem.eth",
-  },
-  {
-    address: "0xbF49B454818783D12Bf4f3375ff17C59015e66Cb",
-    displayName: "Rubber^Duck",
+    {
+    address: "0x8be60fe9F7C8d940D8DA9d5dDD0D8E0c15A4288B",
+    ens: "jvk.eth",
+    displayName: "JVK",
   },
 ];
 export const MEMBER_ADDRESSES_BY_DISPLAY_NAME = MEMBERS.reduce(
